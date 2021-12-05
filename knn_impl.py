4e76ceb0a135a56b628f627a99c5d56233706efd
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-06 00:43:46 +0300

    Logs for KNN

--- knn_impl.py
+++ knn_impl.py
@@ -1,5 +1,7 @@
+import sys
 import pandas as pd
 import numpy as np
+from scipy.spatial.distance import chebyshev
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
@@ -15,47 +17,28 @@ def calculate_target(dataset):
     return result_list
 
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+used_metric     = str(sys.argv[1])
+num_neighbors   = int(sys.argv[2])
+pca_components  = int(sys.argv[3])
 
 df = pd.read_csv('feature_extraction/training.csv')
-
 X = df.drop(columns=['song_name'])  # Keeps all the features of the songs
 
-# print(df.head())
+y = df['song_name'].values 
+y = calculate_target(y)
 
-# print(X.head())
-
-
-y = df['song_name'].values  # .wav
-y = calculate_target(y)  # Encoding the genres
-
-# print(y[100:150])
-#
-# print(X.shape)
-# print(len(y))
-# print(df.shape)
-
-pca = PCA(n_components=48)
+pca = PCA(n_components=pca_components)
 pca.fit(X)
 X = pca.transform(X)
 
 ''' Split dataset into train and test data '''
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, stratify=y)
-
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
 ''' Create KNN classifier '''
-knn = KNeighborsClassifier(n_neighbors=15, metric='braycurtis', metric_params={'VI': np.cov(X_train)})
-
-
+knn = KNeighborsClassifier(n_neighbors=num_neighbors, metric=used_metric)
 
 ''' Fit the classifier to the data '''
 knn.fit(X_train, y_train)
 
-'''show first 5 model predictions on the test data '''
-print('Actual : ', y_test[0:5])
-print('Prediction : ', knn.predict(X_test)[0:5])
-
-print('Score : ', knn.score(X_test, y_test))
+''' Predict the response for test dataset '''
+print(knn.score(X_test, y_test))
