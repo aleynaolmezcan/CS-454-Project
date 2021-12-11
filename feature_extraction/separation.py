0429a78e5d0894bd7fa021f0d93c3315b68da200
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-12-12 00:50:35 +0300

    Labels added to the end

--- feature_extraction/separation.py
+++ feature_extraction/separation.py
@@ -1,7 +1,17 @@
 from random import choices
 
 print('Dont use the script. Files are already separated.')
-exit()
+with open('features_sr_fixed.csv', 'r+') as f:
+    with open('features_last.csv', 'w+') as g:
+        g.write(f.readline().strip() + ",label\n")
+        for line in f:
+            line = line.strip()
+            song_name = line.split(',')[0]
+            label = song_name.split('/')[-2]
+            line += "," + label + "\n"
+            g.write(line)
+
+
 # 0 : Training
 # 1 : Testing
 # 2 : Validation
@@ -9,7 +19,7 @@ population = [0, 1]
 weights    = [0.75, 0.25]
 distribution_samples = choices(population, weights, k=1000)
 
-with open('features.csv', 'r') as f:
+with open('features_sr_fixed.csv', 'r') as f:
     l1 = f.readline()
     with open('training.csv', 'w+') as f_train:
         with open('validation.csv', 'w+') as f_valid:
