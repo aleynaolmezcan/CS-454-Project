9b1fbd5e4ca3ac7b1c5708a964b5bae840a92806
Burcu Arslan <burcu.arslan@ozu.edu.tr>
2022-01-10 23:10:46 -0800

    new logs

--- knn_impl.py
+++ knn_impl.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import numpy as np
+from scipy.spatial import distance
 from sklearn.metrics._plot.confusion_matrix import ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -8,19 +9,19 @@ from sklearn.decomposition import PCA
 from sklearn.neighbors import DistanceMetric
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
 from scipy.spatial.distance import pdist, wminkowski, squareform
+from sklearn.model_selection import GridSearchCV   
 
 
 used_metric     = str(sys.argv[1])
 num_neighbors   = int(sys.argv[2])
 pca_components  = int(sys.argv[3])
-r_states        = int(sys.argv[4])
-
+r_state         = int(sys.argv[4])
 
 df = pd.read_csv('feature_extraction/features_last.csv')
 X = df.drop(columns=['song_name'])  # Keeps all the features of the songs
-scaler = StandardScaler()
+scaler = MinMaxScaler()
 X = scaler.fit_transform(np.array(X.iloc[:, :-1], dtype = float))
 
 
@@ -32,9 +33,18 @@ if pca_components > 0:
     X = pca.transform(X)
 
 ''' Split dataset into train and test data '''
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=r_states, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=r_state)
+X_val, X_test, y_val, y_test = train_test_split(X_test, y_test, test_size=0.5, stratify=y_test, random_state=r_state)
 
-knn = KNeighborsClassifier(n_neighbors=num_neighbors, metric=used_metric)
+#param_grid = {'used_metric' : ['mahalanobis', 'euclidean', 'minkowski', 'manhattan', 'chebyshev', 'hamming'], 'weights' : ['uniform', 'distance'],'n_neighbors': [0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
+
+
+knn = KNeighborsClassifier(metric=used_metric, n_neighbors=num_neighbors)
+# model = GridSearchCV(knn, param_grid, cv=3)
+
+# model.fit(X_train,y_train,groups=None)
+
+
 
 if used_metric == 'mahalanobis':
     knn = KNeighborsClassifier(n_neighbors=num_neighbors, metric=used_metric, metric_params={'VI': np.cov(X_train.T)})
@@ -45,7 +55,9 @@ if used_metric == 'wminkowski':
 ''' Fit the classifier to the data '''
 knn.fit(X_train, y_train)
 #c_mat = confusion_matrix(y_test, knn.predict(X_test))
-print(knn.score(X_test, y_test))
+# print(knn.score(X_val, y_val))
+print(knn.score(X_val, y_val))
+
 
 #disp = ConfusionMatrixDisplay(confusion_matrix=c_mat)
 #disp.plot()
--- process.sh
+++ process.sh
@@ -1,17 +1,17 @@
-echo "Metrics,Num_Neighbors,PCA_Components,N_Random_States,Score" > log/logs4.csv
+echo "Metrics,Num_Neighbors,PCA_Components,N_Random_States,Score" > log/logs8.csv
 
-metrics="wminkowski mahalanobis euclidean minkowski manhattan chebyshev hamming"
+metrics="minkowski mahalanobis euclidean manhattan"
 
 for metric in $metrics
 do
-    for neighbor in 1 2 3 4 5 7 10 13 15 20
+    for neighbor in 1 2 3 4 5 6 7 8 9 10 11 13 15 20
     do
-        for PCA_Components in 0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 20 25 30 35 40 45 50 55 60 65 70 75
+        for PCA_Components in 0 3 4 5 6 7 8 9 10 13 15 20 25 30 35 40 45 50 55 60 65 70 75
         do
-            for state in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+            for state in 1 2 3 5 10 15 20
             do
-                echo -n "${metric},${neighbor},${PCA_Components},${state}," >> log/logs4.csv
-                python knn_impl.py $metric $neighbor $PCA_Components $state >> log/logs4.csv
+                echo -n "${metric},${neighbor},${PCA_Components},${state}," >> log/logs8.csv
+                python knn_impl.py $metric $neighbor $PCA_Components $state >> log/logs8.csv
             done
         done
     done
